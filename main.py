f8089bd128d851ca3a5ac5b2a7f7998bd5a50219
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-06-01 11:35:50 +0600

    functionality to filter by vote and sort them are added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from os import link
 import requests
 from bs4 import BeautifulSoup
+import pprint
+
 
 res = requests.get('https://news.ycombinator.com/')
 data = BeautifulSoup(res.text, 'html.parser')
@@ -8,17 +10,22 @@ links = data.select('.storylink')
 subtext = data.select('.subtext')
 
 
+def sort_by_vote(arr):
+    return sorted(arr, key=lambda k: k['vote'])
+
+
 def create_new_list(links, subtext):
     scraped_data = []
     for idx, item in enumerate(links):
-        title = links[idx].getText()
-        href = links[idx].get('href', None)
+        title = item.getText()
+        href = item.get('href', None)
         vote = subtext[idx].select('.score')
         if(len(vote)):
             points = int(vote[0].getText().replace(' points', ''))
-            scraped_data.append({'title': title, 'url': href})
+            if points > 99:
+                scraped_data.append(
+                    {'title': title, 'url': href, 'vote': points})
+    return sort_by_vote(scraped_data)
 
-    return scraped_data
 
-
-print(create_new_list(links, subtext))
+pprint.pprint(create_new_list(links, subtext))
